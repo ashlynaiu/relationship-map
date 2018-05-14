077fc56bbcf5b01cea0c7406d4115cd6b2082cf5
ashlynaiu <ashlyn.aiu@gmail.com>
2018-05-14 14:48:01 -0700

    clean

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,23 +5,23 @@ class Card extends Component {
   render() {
     const { card } = this.props;
     return (
-        <article className="slds-card">
-            <div className="slds-card__header slds-grid">
-                <header className="slds-media slds-media_center slds-has-flexi-truncate">
-                <div className="slds-media__figure">
-                    <Icon object={card.object}></Icon>
-                </div>
-                <div className="slds-media__body">
-                    <h2 className="slds-card__header-title">
-                    <a href="javascript:void(0);" className="slds-card__header-link slds-truncate" title="Accounts">
-                        <span className="slds-text-heading_small">{card.title}</span>
-                    </a>
-                    </h2>
-                </div>
-                </header>
+      <article className="slds-card">
+        <div className="slds-card__header slds-grid">
+          <header className="slds-media slds-media_center slds-has-flexi-truncate">
+            <div className="slds-media__figure">
+              <Icon object={card.object}></Icon>
             </div>
-            <div className="slds-card__body slds-card__body_inner">{card.info}</div>
-        </article>
+            <div className="slds-media__body">
+              <h2 className="slds-card__header-title">
+                <a href="javascript:void(0);" className="slds-card__header-link slds-truncate" title="Accounts">
+                  <span className="slds-text-heading_small">{card.title}</span>
+                </a>
+              </h2>
+            </div>
+          </header>
+        </div>
+        <div className="slds-card__body slds-card__body_inner">{card.info}</div>
+      </article>
     );
   }
 }
--- src/components/treeLevel.js
+++ src/components/treeLevel.js
@@ -2,22 +2,22 @@
 import React, { Component } from 'react';
 import TreeNode from './treeNode';
 class treeLevel extends Component {
-    render () {
-        const { people, changeCard, isHide } = this.props;
-        
-        return (
-            <div className={`${people[0].start ? '' : 'level'} ${isHide ? 'hide' : ''}`}>
-                {Object
-                    .keys(people)
-                    .map(key => <TreeNode
-                        key={key}
-                        index={key}
-                        changeCard={changeCard}
-                        person={people[key]}/>)
-                }
-            </div>
-        )
-    }
+  render () {
+    const { people, changeCard, isHide } = this.props;
+
+    return (
+      <div className={`${people[0].start ? '' : 'level'} ${isHide ? 'hide' : ''}`}>
+        {Object
+          .keys(people)
+          .map(key => <TreeNode
+            key={key}
+            index={key}
+            changeCard={changeCard}
+            person={people[key]}/>)
+        }
+      </div>
+    )
+  }
 }
 
 export default treeLevel; 
