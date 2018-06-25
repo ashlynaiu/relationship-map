cb85ea9d1d8e3b1e689911f32df39b0aa52e65b6
ashlynaiu <ashlyn.aiu@gmail.com>
2018-06-25 11:45:53 -0700

    remove pop over

--- src/components/treeNode.js
+++ src/components/treeNode.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CrudButtons from './crudButtons';
-import PopOver from './popOver';
 import Icon from './Icon';
 
 
@@ -9,12 +8,10 @@ class treeNode extends Component {
   constructor(props) {
     super();
     this.hoverHandler = this.hoverHandler.bind(this);
-    this.popOverHandler = this.popOverHandler.bind(this);
     this.state = {
       showButtons: false,
       showPopOver: false
     }
-    
   }
 
   hoverHandler() {
@@ -22,16 +19,11 @@ class treeNode extends Component {
     return this.setState({ showButtons: !showButtons });
   }
 
-  popOverHandler() {
-    // let showPopOver = this.state.showPopOver;
-    // return this.setState({ showPopOver: !showPopOver });
-  }
-
   render() {
     const { person, hideChildren } = this.props;
     let renderExtension = () => {
       return(
-        <button className="slds-button slds-button_stateful slds-button_neutral slds-not-selected circle-button-extender slds-is-absolute" aria-live="assertive" onClick={() => hideChildren()} onMouseEnter={this.popOverHandler} onMouseLeave={this.popOverHandler}>{person.children.length}</button>
+        <button className="slds-button slds-button_stateful slds-button_neutral slds-not-selected circle-button-extender slds-is-absolute" aria-live="assertive" onClick={() => hideChildren()} >{person.children.length}</button>
       )
     }
 
@@ -46,7 +38,6 @@ class treeNode extends Component {
                   <span className="node-role">{person.role}</span>
                 </div>
             </div>
-            {person.children !== null && <PopOver showPopOver={this.state.showPopOver} children={person.children}/>}
         </div>
     )
   }
